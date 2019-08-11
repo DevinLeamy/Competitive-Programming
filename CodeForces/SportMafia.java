adfd670ca8c77c9da49fbafe9da53df3378dc235
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-08-11 10:33:23 -0600

    Add files via upload

---
+++ CodeForces/SportMafia.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class SportMafia {
+    private static long solve(long N, long K){
+        long squareRoot = Math.round(Math.sqrt(9 + (-4 * -(2* (N + K)))));
+        return N - ((-3 + squareRoot)/2);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer NK = new StringTokenizer(in.readLine());
+        long N = Long.parseLong(NK.nextToken());
+        long K = Long.parseLong(NK.nextToken());
+        System.out.println(solve(N, K));
+    }
+}
