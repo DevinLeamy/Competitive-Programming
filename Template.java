a50edb945d8d210c07279fbfd80361748d28ca16
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-08-12 14:53:51 -0600

    Add files via upload

---
+++ Template.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Template {
+    public static void main(String[] args) throws IOException {
+
+    }
+    private static StringTokenizer line = new StringTokenizer("");
+    private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    private static int nextInt() throws IOException
+    {
+        return Integer.parseInt(nextString());
+    }
+    private static long nextLong() throws IOException
+    {
+        return Long.parseLong(nextString());
+    }
+    private static String nextString() throws IOException
+    {
+        if (line.hasMoreTokens())
+        {
+            return line.nextToken();
+        } else {
+            line = new StringTokenizer(in.readLine());
+            return line.nextToken();
+        }
+    }
+}
