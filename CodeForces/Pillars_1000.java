7f05f002216e366b2bd9bd4d285180055db8aee3
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-08-13 17:12:32 -0600

    Update and rename Pillars.java to Pillars_1000.java

--- CodeForces/Pillars_1000.java
+++ CodeForces/Pillars_1000.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Pillars {
+public class Pillars_1000 {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(in.readLine());
