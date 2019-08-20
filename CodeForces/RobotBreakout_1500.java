6a55eaeb6fc403a0b5f9c4c204516f183612565d
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-08-19 20:46:22 -0600

    Update and rename RobotBreakout.java to RobotBreakout_1500.java

--- CodeForces/RobotBreakout_1500.java
+++ CodeForces/RobotBreakout_1500.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class RobotBreakout {
+public class RobotBreakout_1500 {
     public static void main(String[] args) throws IOException {
         int T = nextInt();
         for (int i = 0; i < T; i++)
