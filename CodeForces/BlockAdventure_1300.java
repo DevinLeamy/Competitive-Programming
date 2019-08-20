44b48199e864fd974bcfe8c1ba7703966d3b6ccb
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-08-19 20:08:11 -0600

    Update and rename BlockAdventure.java to BlockAdventure_1300.java

--- CodeForces/BlockAdventure_1300.java
+++ CodeForces/BlockAdventure_1300.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class BlockAdventure {
+public class BlockAdventure_1300 {
     public static void main(String[] args) throws IOException {
         int T = nextInt();
         for (int i = 0 ; i < T; i++)
