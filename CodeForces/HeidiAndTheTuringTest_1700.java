b54d1bf188fae1a06443e1b4f14a47c0a96de6c7
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-09-05 08:49:41 -0600

    Update and rename HeidiAndTheTuringTest.java to HeidiAndTheTuringTest_1700.java

--- CodeForces/HeidiAndTheTuringTest_1700.java
+++ CodeForces/HeidiAndTheTuringTest_1700.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class HeidiAndTheTuringTest {
+public class HeidiAndTheTuringTest_1700 {
     public static void main(String[] args) throws IOException
     {
        int N = nextInt();
