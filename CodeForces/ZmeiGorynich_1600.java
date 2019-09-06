628c44638a3832aa31118be7dbdb86c3fdae6124
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-09-06 17:34:27 -0600

    Update and rename ZmeiGorynich.java to ZmeiGorynich_1600.java

--- CodeForces/ZmeiGorynich_1600.java
+++ CodeForces/ZmeiGorynich_1600.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class ZmeiGorynich {
+public class ZmeiGorynich_1600 {
     public static void main(String[] args) throws IOException
     {
         int T = nextInt();
