e76e932ba87f35bd49cfaec9ebc9ca5a8c7ba020
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-10-18 19:28:10 -0600

    Update and rename ProblemH.cpp to DisjointSet.cpp

--- Templates/DisjointSet.cpp
+++ Templates/DisjointSet.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 
 using namespace std;
@@ -6,14 +5,12 @@ class DisjointSet {
       public:
       vector<int> depth;
       vector<int> parents;
-      vector<int> size;
       int N;
       DisjointSet(int n) {
          N = n;
          for (int i = 0; i < N; i++) {
             depth.push_back(0);
             parents.push_back(i);
-            size.push_back(1);
          }
       }
       int findSet(int x) {
@@ -22,43 +19,19 @@ class DisjointSet {
          }
          return parents[x];
       }
-      int unionSets(int x, int y) {
+      void unionSets(int x, int y) {
          int xRep = findSet(x);
          int yRep = findSet(y);
          if (xRep == yRep) {
-            return xRep;
+            return;
          }
-         int temp = size[xRep];
-         size[xRep] += size[yRep];
-         size[yRep] += temp;
          if (depth[xRep] > depth[yRep]) {
             parents[yRep] = xRep;
-            return xRep;
          } else if (depth[yRep] > depth[xRep]) {
             parents[xRep] = yRep;
-            return yRep;
          } else {
             depth[xRep] += 1;
             parents[yRep] = xRep;
-            return xRep;
          }
       }
 };
-int main() {
-   int N, L, A, B, R;
-   cin >> N >> L;
-   DisjointSet groups = DisjointSet(L);
-   for (int i = 0; i < N; i++) {
-      cin >> A >> B;
-      A--;
-      B--;
-      R = groups.unionSets(A, B);
-      if (groups.size[R] <= 0) {
-         cout << "SMECE" << endl;
-      } else {
-         groups.size[R]--;
-         cout << "LADICA" << endl;
-      }
-   }
-   return 0;
-}
