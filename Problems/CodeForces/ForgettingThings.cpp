529863d71af13e821f99c9275207efba66456e4f
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-10-26 20:34:15 -0600

    Add files via upload

---
+++ Problems/CodeForces/ForgettingThings.cpp
@@ -0,0 +1,25 @@
+//Not on github
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int a, b;
+    scanf("%i%i", &a, &b);
+    if (b < a || b - a > 1) {
+        if (a == 9 && b == 1) {
+            cout << a << 9 << " " << b << 0 << 0 << endl;
+        } else {
+            cout << -1 << endl;
+        }
+    } else {
+        if (a == b) {
+            cout << a << 0 << " " << b << 1 << endl;
+        } else {
+            cout << a << 9 << " " << b << 0 << endl;
+        }
+    }
+    return 0;
+}
