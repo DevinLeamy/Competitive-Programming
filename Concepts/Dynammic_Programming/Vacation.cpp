eb34e6a5a03a8646586f570835bd89592e7a236c
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-10-28 16:02:56 -0600

    Update Vacation.cpp

--- Concepts/Dynammic_Programming/Vacation.cpp
+++ Concepts/Dynammic_Programming/Vacation.cpp
@@ -3,41 +3,25 @@
 
 using namespace std;
 
-int main()
-{
-    int N, A;
-    cin >> N;
-    vector<long long> dp(3, 0);
-    for (int i = 0; i < N; i++)
-    {
-        vector<int> activities(3);
-        vector<long long> new_dp(3);
-        for (int i = 0; i < 3; i++)
-        {
-            cin >> A;
-            activities[i] = A;
-        }
-        for (int j = 0; j < 3; j++)
-        {
-            for (int z = 0; z < 3; z++)
-            {
-                if (j != z)
-                {
-                    new_dp[j] = max(new_dp[j], dp[z] + activities[j]);
-                }
-            }
-        }
-        for (int j = 0; j < 3; j++)
-        {
-            dp[j] = new_dp[j];
-        }
+int days[100001][3], dp[100001][3];
+int INF = 10e8 + 1;
+int main() {
+    int a, b, c, n;
+    scanf("%i", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%i%i%i", &a, &b, &c);
+        days[i][0] = a;
+        days[i][1] = b;
+        days[i][2] = c;
     }
-    long long maxV = -1;
-    for (long long value : dp)
-    {
-        maxV = max(maxV, value);
+    dp[0][0] = days[0][0];
+    dp[0][1] = days[0][1];
+    dp[0][2] = days[0][2];
+    for (int i = 1; i < n; i++) {
+        dp[i][0] = max(dp[i-1][1], dp[i-1][2]) + days[i][0];
+        dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + days[i][1];
+        dp[i][2] = max(dp[i-1][1], dp[i-1][0]) + days[i][2];
     }
-    cout << maxV;
-
+    cout << max( max(dp[n-1][0], dp[n-1][1]), dp[n-1][2]) << endl;
     return 0;
 }
