305dde78836f5c1f464d23333b665cb1938a9789
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-10-28 16:01:56 -0600

    Update Knapsack1.cpp

--- Concepts/Dynammic_Programming/Knapsack1.cpp
+++ Concepts/Dynammic_Programming/Knapsack1.cpp
@@ -1,30 +1,23 @@
 #include <iostream>
 #include <vector>
 
-
 using namespace std;
 
-int main()
-{
-    int N, W, weight;
-    long long value;
-    cin >> N >> W;
-
-    vector<long long> dp(W+1);
-
-    for (int i = 0; i < N; i++)
-    {
-        cin >> weight >> value;
-        for (int j = W-weight; j >= 0; j--)
-        {
-            dp[j + weight] = max(dp[j + weight], dp[j] + value);
+int INF = 10e8 + 1;
+long long dp[(int)10e5+1];
+int main() {
+    int N, W, w, v;
+    scanf("%i%i", &N, &W);
+    vector< pair<int, int> > items;
+    for (int i = 0; i < N; i++) {
+        scanf("%i%i", &w, &v);
+        items.push_back({w, v});
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = W; j >= items[i].first; j--) {
+            dp[j] = max(max(dp[j-1], dp[j]), dp[j-items[i].first] + items[i].second);
         }
     }
-    long long answer = 0;
-    for (int i = 0; i <= W; i++)
-    {
-        answer = max(answer, dp[i]);
-    }
-    cout << answer;
+    cout << dp[W] << endl;
     return 0;
 }
