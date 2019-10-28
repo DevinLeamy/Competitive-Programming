91346e85020d9f5f5fa59f935c23fd63828dc6b9
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-10-28 16:01:02 -0600

    Update LCS.cpp

--- Concepts/Dynammic_Programming/LCS.cpp
+++ Concepts/Dynammic_Programming/LCS.cpp
@@ -1,41 +1,23 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-
-string getLongestString(string a, string b)
-{
-    if (a.length() >= b.length())
-    {
-        return a;
-    }
-    return b;
-}
-string getLCS(string string1, string string2)
-{
-    string dp[string1.length()+1][string2.length()+1];
-    for (int i = 1; i <= string1.length(); i++)
-    {
-        for (int j = 1; j <= string2.length(); j++)
-        {
-            if (string1.at(i-1) == string2.at(j-1))
-            {
-                dp[i][j] = getLongestString(dp[i-1][j-1], dp[i][j-1]) + string1.at(i-1);
-            } else {
-                dp[i][j] = getLongestString(dp[i-1][j-1], dp[i][j-1]);
+string dp[3001][3001];
+int main() {
+    ios_base::sync_with_stdio(0);
+    string top, bottom;
+    cin >> top >> bottom;
+    for (int i = 1; i <= top.length(); i++) {
+        for (int j = 1; j <= bottom.length(); j++) {
+            if (top[i-1] == bottom[j-1]) {
+                dp[i][j] += top[i-1];
             }
+            string newString;
+            newString = dp[i-1][j];
+            if (dp[i][j-1].length() > newString.length()) {newString = dp[i][j-1];}
+            if ((dp[i-1][j-1] + dp[i][j]).length() > newString.length()) {newString = dp[i-1][j-1] + dp[i][j];}
+            dp[i][j] = newString;
         }
     }
-    return dp[string1.length()][string2.length()];
-}
-
-int main()
-{
-    string string1, string2;
-    cin >> string1 >> string2;
-    string output = getLCS(string1, string2);
-    cout << output << endl;
-
-    return 0;
+    cout << dp[top.length()][bottom.length()] << endl;
 }
