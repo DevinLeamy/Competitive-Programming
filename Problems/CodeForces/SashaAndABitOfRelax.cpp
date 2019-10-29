143eec4c546e5bb50ef92c7575b78b9532b69736
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-10-28 20:51:13 -0600

    Add files via upload

---
+++ Problems/CodeForces/SashaAndABitOfRelax.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int dp[2][1048577];
+long long values[300001];
+int main() {
+    ios_base::sync_with_stdio(0);
+    int n;
+    long long a;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        values[i] = a;
+    }
+    long long result = 0;
+    int x = 0;
+    dp[1][0] = 1;
+    for (int i = 0; i < n; i++) {
+        x ^= values[i];
+        result += dp[i%2][x];
+        dp[i%2][x]++;
+    }
+    cout << result << endl;
+}
+//if dp[i] - dp[i-n] == 0 and n % 2 == 1 then (i, i-n) is a funny pair
+//Ya did good but you missed one key observation. (2/3)
