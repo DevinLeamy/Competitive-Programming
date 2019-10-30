6b2d650e8a5d1024ed52dfa0ca2b19b0cdbf422c
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2019-10-29 19:59:01 -0600

    Update FlippingGame_1200.cpp

--- Problems/CodeForces/FlippingGame_1200.cpp
+++ Problems/CodeForces/FlippingGame_1200.cpp
@@ -1,30 +1,16 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(0);
     int n, h, initial = 0;
     cin >> n;
-    vector<int> game(n);
+    int output = -1, sum = 0;
     for (int i = 0; i < n; i++) {
         cin >> h;
-        if (h == 1) {initial++;}
-        game[i] = h;
+        sum = max(((h==0)? 1 : -1), sum + ((h==0)? 1 : -1));
+        output = max(sum, output);
+        initial += ((h==0)? 0 : 1);
     }
-    if (n == 1) {printf("%i", (1 - initial));}
-    else {
-        int output = -1;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                int count = 0;
-                for (int z = i; z <= j; z++) {
-                    if (game[z] == 0) {count++;}
-                    else {count--;}
-                }
-                output = max(count, output);
-            }
-        }
-        printf("%i", (output + initial));
-    }
+    printf("%i", (output + initial));
 }
