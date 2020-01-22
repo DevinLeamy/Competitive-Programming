1270c9523921ea5568856287e9163084bc5f014d
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2020-01-21 20:38:29 -0700

    Add files via upload

---
+++ Templates/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#define ll long long
+#define pb push_back
+#define doH(h) for(int i = 0; i < h; i++)
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	
+	return 0;
+}
