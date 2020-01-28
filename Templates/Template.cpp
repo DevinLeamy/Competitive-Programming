ba305c9fcfe98b5794fce71ca45c50215358f310
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2020-01-27 19:19:57 -0700

    Update Template.cpp

--- Templates/Template.cpp
+++ Templates/Template.cpp
@@ -2,14 +2,20 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <queue>
+#include <set>
+#include <map>
+#define INF 2147483647;
+#define LINF 9223372036854775807;
+#define PI pair<int, int>
 #define ll long long
 #define pb push_back
-#define doH(h) for(int i = 0; i < h; i++)
+#define loop(h) for(int i = 0; i < h; i++)
 
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(0);
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	
 	return 0;
 }
