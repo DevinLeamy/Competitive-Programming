c365114794e9ddc06f5f31ad5237263831fe613f
DevinLeamy <45083086+DevinLeamy@users.noreply.github.com>
2020-02-01 22:29:41 -0700

    Update Template.cpp

--- Toolkit/Templates/Template.cpp
+++ Toolkit/Templates/Template.cpp
@@ -7,12 +7,13 @@
 #include <map>
 #define INF 2147483647;
 #define LINF 9223372036854775807;
+#define scan(x) do{while((x=getchar_unlocked())<'0'); for(x-='0'; '0'<=(_=getchar_unlocked()); x=(x<<3)+(x<<1)+_-'0');}while(0)
 #define PI pair<int, int>
 #define ll long long
 #define pb push_back
 #define loop(h) for(int i = 0; i < h; i++)
-
 using namespace std;
+char _;
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
