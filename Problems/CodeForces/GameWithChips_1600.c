b4af22b2ea8a81198169b71ff8f0a0b425d63f4b
DevinLeamy <devinleamy@gmail.com>
2020-04-19 19:59:59 -0600

    shortend

--- Problems/CodeForces/GameWithChips_1600.c
+++ Problems/CodeForces/GameWithChips_1600.c
@@ -14,47 +14,24 @@ int main() {
                 printf("0");
                 return 0;
         }
-        if (n < m) {
-                int count = n + m + (n * m) + n - 3;
-                printf("%d\n", count);
-                for (int i = 0; i < n-1; i++) {
-                        printf("U");
-                }
-                for (int i = 0; i < m-1; i++) {
-                        printf("L");
-                }
-                for (int i = 0; i < n; i++) {
-                        for (int j = 0; j < m; j++) {
-                                if (i % 2 == 0) {
-                                        printf("R");
-                                } else {
-                                        printf("L");
-                                }
-                        }
-                        if (i != n-1) {
-                                printf("D");
-                        }
-                }
-        } else {
-                int count = m + n + (n * m) + m - 3;
-                printf("%d\n", count);
-                for (int i = 0; i < n-1; i++) {
-                        printf("U");
-                }
-                for (int i = 0; i < m-1; i++) {
-                        printf("L");
-                }
-                for (int i = 0; i < m; i++) {
-                        for (int j = 0; j < n; j++) {
-                                if (i % 2 == 0) {
-                                        printf("D");
-                                } else {
-                                        printf("U");
-                                }
-                        }
-                        if (i != m-1) {
+        int count = n + m + (n * (m-1)) + n - 3;
+        printf("%d\n", count);
+        for (int i = 0; i < n-1; i++) {
+                printf("U");
+        }
+        for (int i = 0; i < m-1; i++) {
+                printf("L");
+        }
+        for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m-1; j++) {
+                        if (i % 2 == 0) {
                                 printf("R");
+                        } else {
+                                printf("L");
                         }
+                }
+                if (i != n-1) {
+                        printf("D");
                 }
         }
 }
