bec0aff74e41b0310bd33ae306bc8c0b711d3aac
DevinLeamy <devinleamy@gmail.com>
2020-04-22 15:00:42 -0600

    edited

--- Toolkit/Templates/Template.cpp
+++ Toolkit/Templates/Template.cpp
@@ -6,8 +6,8 @@
 #include <set>
 #include <map>
 #include <iomanip>
-#define INF 2147483647;
-#define LINF 9223372036854775807;
+#define INF 2147483647
+#define LINF 9223372036854775807ll
 #define scan(x) do{while((x=getchar_unlocked())<'0'); for(x-='0'; '0'<=(_=getchar_unlocked()); x=(x<<3)+(x<<1)+_-'0');}while(0)
 #define PI pair<int, int>
 #define ll long long
