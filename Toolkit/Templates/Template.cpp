57a4a0ceec3b8fc15a83933957614572389ccad1
DevinLeamy <devinleamy@gmail.com>
2020-04-23 21:01:49 -0600

    Added Macros

--- Toolkit/Templates/Template.cpp
+++ Toolkit/Templates/Template.cpp
@@ -5,11 +5,13 @@
 #include <queue>
 #include <set>
 #include <map>
+#include <string>
 #include <iomanip>
 #define INF 2147483647
 #define LINF 9223372036854775807ll
 #define scan(x) do{while((x=getchar_unlocked())<'0'); for(x-='0'; '0'<=(_=getchar_unlocked()); x=(x<<3)+(x<<1)+_-'0');}while(0)
 #define PI pair<int, int>
+#define PL pair<long long, long long>
 #define ll long long
 #define pb push_back
 #define loop(h) for(int i = 0; i < h; i++)
