3a829e112f6e026c63fec00725a717fee8cf3c57
DevinLeamy <devinleamy@gmail.com>
2020-06-03 10:56:52 -0600

    Add files via upload

---
+++ Problems/CodeForces/ParkingLight.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <iomanip>
+#define INF 2147483647
+#define LINF 9223372036854775807ll
+#define scan(x) do{while((x=getchar_unlocked())<'0'); for(x-='0'; '0'<=(_=getchar_unlocked()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+#define PI pair<int, int>
+#define PL pair<long long, long long>
+#define ll long long
+#define pb push_back
+#define loop(h) for(int i = 0; i < h; i++)
+using namespace std;
+char _;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t; cin >> t;
+	loop(t) {
+		double n, m; cin >> n >> m;
+		int count = 0;
+		int nextRow = n;
+		while (m--) {
+			count += (nextRow / 2) + (nextRow % 2);
+			nextRow = (n) - (nextRow % 2);
+		}
+		cout << count << endl;
+	}
+	return 0;
+}
