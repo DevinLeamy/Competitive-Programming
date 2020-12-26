85c0fa02dbe6fb2334a36040c67c4ead55675427
Devin Leamy <devinleamy@gmail.com>
2020-12-26 15:44:54 -0700

    Create LongestPalindromicPrefix.cpp

---
+++ Toolkit/Algorithms/LongestPalindromicPrefix.cpp
@@ -0,0 +1,23 @@
+// KMP Algorithm for Longest Palindromic Prefix
+
+int long_pal_pre(string s) {
+	string _s = s; reverse(_s.begin(), _s.end());
+	string s2 = s + "?" + _s;
+	int n = (int) s2.length();
+	vector<int> lps(n);
+	for (int i = 1; i < n; i++) {
+		int len = lps[i-1];
+		while (len > 0 && s2[len] != s2[i]) {
+			len = lps[len - 1];
+		}
+		if (s2[i] == s2[len]) len++;
+		lps[i] = len;
+	}
+	return lps.back();
+}
+
+int main() {
+  string test = "abccbagg";
+  string result = long_pal_pre(test);
+  assert(result == "abccba");
+}
